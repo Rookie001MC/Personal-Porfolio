4a0e211e084a3bbd17a8440e42df9544e8ec3b00
Nguyễn Hữu Quốc Thắng <nguyenquocthang2004@gmail.com>
2021-02-15 21:17:01 +0700

    Fade in animations to both pages + crude auto navbar hide

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-var counter = 1; //instantiate a counter
-var maxImage =3; //the total number of images that are available
-setInterval(function() {
-    document.querySelector('header').style.backgroundImage = "url('images/" + (counter + 1) + ".jpg')";
-    if (counter + 1 == maxImage) {
-        counter = 0; //reset to start
-    } else {
-        ++counter; //iterate to next image
-    }
-}, 10000);
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("navbar")[0].style.display = "flex"
+  } else {
+    document.getElementsByClassName("navbar")[0].style.display ="none"
+  }
+  prevScrollpos = currentScrollPos;
+}
